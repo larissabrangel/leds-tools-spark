18f71e4d399f3c65a1883f64f241699ae0e76c5c
Nicolas <nicolasbotelho2005@gmail.com>
2025-05-27 19:19:15 -0300

    fix(GESTAO-n): #comment: Conclusao dos testes de objetos

--- test_objects/objects.test.ts
+++ test_objects/objects.test.ts
@@ -1,6 +1,6 @@
 import { beforeAll, expect, test } from "vitest"
 import { fileToPackages } from "../src/cli/frontend/vue-vite/generate.js"
-import { Model } from "../src/language/generated/ast.js"
+import { isLocalEntity, isModule, LocalEntity, Model } from "../src/language/generated/ast.js"
 import { parseHelper } from "langium/test";
 import { createSPARKServices } from "../src/language/s-p-a-r-k-module.js";
 import { EmptyFileSystem, LangiumDocument } from "langium";
@@ -94,19 +94,13 @@ module Test {
 }
 `)
     
-    // const listPackages: PackageAbstraction[] = fileToPackages(document?.parseResult.value)
-    
-    // const listPackages = getPackages(document?.parseResult.value)
     const model = document?.parseResult.value
-    const absElemList = model.abstractElements
     const clsList: LocalEntity[] = []
 
     for (const absElem of model.abstractElements) {
         if (isModule(absElem)) {
             for (const elem of absElem.elements) {
-                if (isLocalEntity(elem)) {
-                    clsList.push(elem)
-                }
+                if (isLocalEntity(elem)) clsList.push(elem)
             }
         }
     }
@@ -119,40 +113,25 @@ module Test {
     expect(attr1Cls1.name).toBe("nome")
     expect(attr1Cls1.type.toString()).toBe("string")
 
-    const attr2Cls1 = attrListCls1[0]
+    const attr2Cls1 = attrListCls1[1]
     expect(attr2Cls1.name).toBe("numero")
     expect(attr2Cls1.type.toString()).toBe("integer")
 
-    const cls2Cls2 = clsList[0]
-    expect(cls1.name).toBe("Entidade2")
+    const relListCls1 = cls1.relations
+    const rel1Cls1 = relListCls1[0]
+    expect(rel1Cls1.$type.toString()).toBe("OneToOne")
+    expect(rel1Cls1.type.ref?.name).toBe("Entidade2")
 
-    const attrListCls2 = cls1.attributes
-    const attr1Cls2 = attrListCls1[0]
+    const cls2 = clsList[1]
+    expect(cls2.name).toBe("Entidade2")
+
+    const attrListCls2 = cls2.attributes
+    const attr1Cls2 = attrListCls2[0]
     expect(attr1Cls2.name).toBe("nome")
     expect(attr1Cls2.type.toString()).toBe("string")
-
-    const attr2Cls2 = attrListCls1[0]
+    const attr2Cls2 = attrListCls2[1]
     expect(attr2Cls2.name).toBe("verificacao")
     expect(attr2Cls2.type.toString()).toBe("boolean")
-
-    //  for (const pack of listPackages) {
-    //     let listCls = pack.getPackageLevelClasses()
-    //     expect(listCls[0].getName()).toBe("Entidade1")
-    //     expect(listCls[1].getName()).toBe("Entidade2")
-
-    //     let listAttrCls1 = listCls[0].getAttributes()
-    //     expect(listAttrCls1[0].getName()).toBe("nome")
-    //     expect(listAttrCls1[0].getType().getName()).toBe("string")
-    //     expect(listAttrCls1[1].getName()).toBe("numero")
-    //     expect(listAttrCls1[1].getType().getName()).toBe("integer")
-    //     console.log(listAttrCls1[2].getName())
-
-    //     let listAttrCls2 = listCls[1].getAttributes()
-    //     expect(listAttrCls2[0].getName()).toBe("nome")
-    //     expect(listAttrCls2[0].getType().getName()).toBe("string")
-    //     expect(listAttrCls2[1].getName()).toBe("verificacao")
-    //     expect(listAttrCls2[1].getType().getName()).toBe("boolean")
-    // }
 })
 
 // config name
