90db24fc18b3e8bc49c85c74bd5f2b2072bd5ca8
Nicolas <nicolasbotelho2005@gmail.com>
2025-06-04 14:48:29 -0300

    fix(GESTAO-n): #comment: correcao na funcao fileToPackages em razão da atualização da SEONLibImplementation  #workflow transition: testing

--- src/cli/frontend/vue-vite/generate.ts
+++ src/cli/frontend/vue-vite/generate.ts
@@ -5,7 +5,7 @@ import { createPath } from "../../util/generator-utils.js";
 import { generate as helpersGenerator } from "./helpers-generator.js";
 import { generate as publicGenerator } from "./public/generate.js";
 import { generate as srcGenerator } from "./src/generate.js";
-import { TypeScriptAttribute, ClassAbstraction, PackageAbstraction, PrimitiveTypeAbstraction, ProjectAbstraction, vueModularArchProjectSettings } from "seon-lib-implementation";
+import SEON from "seon-lib-implementation"
 
 export function generate(model: Model, target_folder: string) : void {
 
@@ -17,7 +17,7 @@ export function generate(model: Model, target_folder: string) : void {
     const listPkg = fileToPackages(model)
  
     // using the lib
-    let project = new ProjectAbstraction(softwareName, softwareDescription, vueModularArchProjectSettings, listPkg);
+    let project = new SEON.ProjectAbstraction(softwareName, softwareDescription, SEON.vueModularArchProjectSettings, listPkg);
 
     project.createProject();
 
@@ -46,13 +46,12 @@ export function fileToPackages(model: Model) {
 
                     for (const attr of elem.attributes) {
                         if (isAttribute(attr)) {
-                            listAttr.push(new TypeScriptAttribute(attr.name, new PrimitiveTypeAbstraction(attr.type.toString())))
+                            listAttr.push(new SEON.TypeScriptAttribute(attr.name, new SEON.PrimitiveTypeAbstraction(attr.type.toString())))
                         }
                     }
+                    const cls = new SEON.ClassAbstraction(elem.name, [], listAttr)
 
-                    const cls = new ClassAbstraction(elem.name, [], listAttr)
-
-                    listPackages.push(new PackageAbstraction(elem.name, [cls], []))
+                    listPackages.push(new SEON.PackageAbstraction(elem.name, [cls], []))
                 }
             }
         }
